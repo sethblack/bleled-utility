3b18552649b8e746c636f66a471e10ca69a5d5a4
Seth Black <sblack@sethserver.com>
2016-03-23 19:51:59 -0500

    created blank project

---
+++ bleled-utility/bleled-utility/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bleled-utility
+//
+//  Created by William Black on 3/23/16.
+//  Copyright © 2016 William Black. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ bleled-utility/bleled-utility/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  bleled-utility
+//
+//  Created by William Black on 3/23/16.
+//  Copyright © 2016 William Black. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
